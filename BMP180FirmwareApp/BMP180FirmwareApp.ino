50baeb3dc5d6bad806825b111fca4db738ae23d4
mstukolov <maxim.stukolov@gmail.com>
2017-03-07 11:14:30 +0300

    init

---
+++ BMP180FirmwareApp/BMP180FirmwareApp.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		BMP180FirmwareApp.ino
+ Created:	3/7/2017 11:04:09 AM
+ Author:	Maxim
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+	Serial.println('cccc');
+	delay(5000);
+  
+}
